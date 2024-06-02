713dbcba2034752f5ed28e85093935aae5782518
Beka-l3 <b.talgat@innopolis.university>
2024-06-02 17:50:15 +0300

    Create ScrollView

--- Mobile/Thesis SwiftUI/JtoS/Model/Extension/Params+Types.swift
+++ Mobile/Thesis SwiftUI/JtoS/Model/Extension/Params+Types.swift
@@ -117,10 +117,12 @@ struct ParamsZStack: JtoSParams {
 struct ParamsScrollView: JtoSParams {
 
     let spacing: CGFloat
+    let axes: Axis.Set
 
     // MARK: Init
 
     init(params: Params) {
         self.spacing = CGFloat(params.spacing ?? 0)
+        self.axes = params.axes == "horizontal" ? .horizontal : .vertical
     }
 }
